82972d60243ef84240dd47942cd6528a1e265283
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-02-03 08:51:03 +0900

    Added clean rotted token function

--- token/server/clean.go
+++ token/server/clean.go
@@ -1,5 +1,19 @@
 package server
 
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
 // CleanRottedToken removes rotted token from the DB.
 func (me *Server) CleanRottedToken() {
+	ctx, stop := me.TimeoutContext(context.Background())
+	defer stop()
+	me.adapter.DeleteMany(ctx, bson.M{
+		"expires": bson.M{
+			"$lt": time.Now().UTC(),
+		},
+	})
 }
