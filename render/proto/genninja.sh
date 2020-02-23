731c68e000f9f492a4d2acba2cc7353ebe2d7ef2
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-02-23 16:22:46 +0900

    Added python rendering skelton

--- render/proto/genninja.sh
+++ render/proto/genninja.sh
@@ -7,11 +7,15 @@ packagePath="github.com/hiroaki-yamamoto/render"
 cat > $(dirname $0)/build.ninja << EOF
 goFlags = --go_out=plugins=grpc
 goOut = $(realpath $(dirname $DIR)/go/rpc --relative-to=$DIR)
+pyOut = $(realpath $(dirname $DIR)/py/rpc --relative-to=$DIR)
 
 inc = .
 
 rule protoc
   command = protoc \$flags:\$outdir -I \$inc \$in
+
+rule genPy
+  command = $VIRTUAL_ENV/bin/python -m grpc_tools.protoc -I\$inc --python_out=\$outdir --grpc_python_out=\$outdir \$in
 
 rule mv
   command = mv \$in \$out
@@ -29,5 +33,15 @@ build tmp/${packagePath}/go/rpc/$childDir/${fname}.pb.go: protoc $(
   outdir = tmp
 
 build \$goOut/$childDir/${fname}.pb.go: mv tmp/${packagePath}/go/rpc/$childDir/${fname}.pb.go
+EOF
+
+pyOutFiles=(
+  "\$pyOut/$childDir/${fname}_pb2.py"
+  "\$pyOut/$childDir/${fname}_pb2_grpc.py"
+)
+cat >> $(dirname $0)/build.ninja << EOF
+build ${pyOutFiles[@]}: genPy $(realpath $f --relative-to=$DIR)
+  flags = \$pyFlags
+  outdir = \$pyOut
 EOF
 done
