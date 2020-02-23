3829462effc20ede2f080313b5bf93d4dc39420f
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-02-23 12:59:06 +0900

    Prepare for pug-based & jinja2-based templates

--- render/proto/genninja.sh
+++ render/proto/genninja.sh
@@ -2,13 +2,12 @@
 # -*- coding: utf-8 -*-
 
 DIR=$(dirname $(realpath $0))
-packagePath="github.com/hiroaki-yamamoto/render/rpc"
+packagePath="github.com/hiroaki-yamamoto/render"
 
 cat > $(dirname $0)/build.ninja << EOF
-backFlags = --go_out=plugins=grpc
-backOut = $(
-  realpath $(dirname $DIR)/rpc --relative-to=$DIR
-)
+goFlags = --go_out=plugins=grpc
+goOut = $(realpath $(dirname $DIR)/go/rpc --relative-to=$DIR)
+
 inc = .
 
 rule protoc
@@ -23,15 +22,12 @@ fname=$(basename -s '.proto' ${f})
 childDir=$(realpath $(dirname ${f}) --relative-to=$DIR)
 
 cat >> $(dirname $0)/build.ninja << EOF
-
-build tmp/${packagePath}/$childDir/${fname}.pb.go: protoc $(
+build tmp/${packagePath}/go/rpc/$childDir/${fname}.pb.go: protoc $(
   realpath $f --relative-to=$DIR
 )
-  flags = \$backFlags
+  flags = \$goFlags
   outdir = tmp
 
-build \$backOut/$childDir/${fname}.pb.go: mv tmp/${packagePath}/$childDir/${fname}.pb.go
-
-$frontSc
+build \$goOut/$childDir/${fname}.pb.go: mv tmp/${packagePath}/go/rpc/$childDir/${fname}.pb.go
 EOF
 done
