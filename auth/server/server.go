36ec9c863d54b570627aaebff984159d7a1508ce
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-04-09 21:06:19 +0900

    Added NewPublicServer func

--- auth/server/server.go
+++ auth/server/server.go
@@ -1,5 +1,13 @@
 package server
 
-// Server represents an auth server.
-type Server struct {
+import "github.com/hiroaki-yamamoto/reusable-services/auth/crypto"
+
+// PublicServer represents an auth server.
+type PublicServer struct {
+	PWHashAlgo []crypto.HashFunc
+}
+
+// NewPublicServer creates a new isntance of Server
+func NewPublicServer(hashAlgo []crypto.HashFunc) *PublicServer {
+	return &PublicServer{PWHashAlgo: hashAlgo}
 }
