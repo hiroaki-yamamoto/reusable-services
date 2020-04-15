96986b780a3dffcdd59b44b1af3331a6146e3226
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-04-15 19:19:27 +0900

    Changed dbunique to use zap

--- auth/vldfuncs/dbunique.go
+++ auth/vldfuncs/dbunique.go
@@ -2,22 +2,22 @@ package vldfuncs
 
 import (
 	"context"
-	"log"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/hiroaki-yamamoto/reusable-services/adapter"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.uber.org/zap"
 )
 
 // DBUnique checks whether the value is unique on the db.
-func DBUnique(adapter adapter.IAdapter) validator.FuncCtx {
+func DBUnique(logger *zap.Logger, adapter adapter.IAdapter) validator.FuncCtx {
 	return func(ctx context.Context, fl validator.FieldLevel) bool {
 		fieldName := fl.FieldName()
 		query := make(bson.M)
 		query[fieldName] = fl.Field().Interface()
 		count, err := adapter.Count(ctx, query)
 		if err != nil {
-			log.Println("[Error]", err)
+			logger.Error("DBUnique has error", zap.Error(err))
 		}
 		return count < 1
 	}
