c290dd1f99fd6a8ea410646df542175c9d48fa13
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2020-04-26 11:42:14 +0900

    Added mocks + test skelton

---
+++ auth/server/server_suite_test.go
@@ -0,0 +1,17 @@
+package server_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestServer(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Server Suite")
+}
+
+var _ = BeforeEach(func() {
+
+})
--- render/proto/genninja.sh
+++ render/proto/genninja.sh
@@ -14,11 +14,11 @@ inc = .
 rule protoc
   command = protoc \$flags:\$outdir -I \$inc \$in
 
-rule genPy
-  command = $VIRTUAL_ENV/bin/python -m grpc_tools.protoc -I\$inc --python_out=\$outdir --grpc_python_out=\$outdir \$in
+rule mock
+  command = mockgen -package mocks -destination \$out -source \$in
 
-rule mv
-  command = mv \$in \$out
+rule genPy
+  command = $(realpath -s $VIRTUAL_ENV/bin/python --relative-to=$DIR) -m grpc_tools.protoc -I\$inc --python_out=\$outdir --grpc_python_out=\$outdir \$in
 
 rule getInitPy
   command = cp init.py \$out
@@ -36,13 +36,10 @@ fname=$(basename -s '.proto' ${f})
 childDir=$(realpath $(dirname ${f}) --relative-to=$DIR)
 
 cat >> $(dirname $0)/build.ninja << EOF
-build tmp/${packagePath}/go/rpc/$childDir/${fname}.pb.go: protoc $(
-  realpath $f --relative-to=$DIR
-)
+build \$goOut/$childDir/${fname}.pb.go: protoc $(realpath $f --relative-to=$DIR)
   flags = \$goFlags
-  outdir = tmp/${packagePath}/go/rpc
-
-build \$goOut/$childDir/${fname}.pb.go: mv tmp/${packagePath}/go/rpc/$childDir/${fname}.pb.go
+  outdir = \$goOut/$childDir
+build \$goOut/mocks/$childDir/${fname}.go: mock \$goOut/$childDir/${fname}.pb.go
 EOF
 
 pyOutFiles=(
